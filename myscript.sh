627eae45f0166994fc2cec03fc041dd235773b9c
Abhishek <abhimudda2003@gmail.com>
2025-08-11 16:03:12 +0530

    Intial commit

---
+++ myscript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cp app1.py app.py
+docker build -t istio-image1 .
+
+cp app2.py app.py
+docker build -t istio-image2 .
+
+cp app3.py app.py
+docker build -t istio-image3 .
+
+docker images
+
+kubectl apply -f deployment1.yml
+kubectl apply -f deployment2.yml
+kubectl apply -f deployment3.yml
+
+kubectl apply -f service.yml
+
+kubectl apply -f virtualservice.yml
+kubectl apply -f gateway.yml
+kubectl apply -f destinationrule.yml
+
+kubectl get deploy
+kubectl get svc
+kubectl get virtualservice
+kubectl get gateway
+kubectl get destinationrule
