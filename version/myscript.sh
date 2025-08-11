d3e7a1ccb407866623a9d600988c3ddb174850a7
Abhishek <abhimudda2003@gmail.com>
2025-08-11 23:21:17 +0530

    .

--- version/myscript.sh
+++ version/myscript.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 cp app1.py app.py
 docker build -t istio-image1:v1 .
-
+rm app.py
 cp app2.py app.py
 docker build -t istio-image2:v2 .
-
+rm app.py
 cp app3.py app.py
 docker build -t istio-image3:v3 .
-
-docker images
-
+rm app.py
 kubectl apply -f deployment1.yml
 kubectl apply -f deployment2.yml
 kubectl apply -f deployment3.yml
@@ -19,6 +17,9 @@ kubectl apply -f service.yml
 kubectl apply -f virtualservice.yml
 kubectl apply -f gateway.yml
 kubectl apply -f destinationrule.yml
+
+
+docker images
 
 kubectl get deploy
 kubectl get svc
